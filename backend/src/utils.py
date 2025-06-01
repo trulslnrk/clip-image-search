d199ca850e216129fdf40d525e03a35180a089e4
trulsnrk <truls.opsjon.lindmark@nrk.no>
2025-06-01 19:02:59 +0200

    Add documentation for utils

--- backend/src/utils.py
+++ backend/src/utils.py
@@ -8,6 +8,14 @@ def ensure_directory(path):
 def normalize(x: np.ndarray) -> np.ndarray:
     return x / np.linalg.norm(x)
 
-# Normalize value within min-max to range [0,1000]
 def normalizeValueWithinRange(min, max, value):
-  return ((value - min) / (max - min)) * 1000
+    """
+    Normalizes a given value within a specified range [min, max] to a scale of 0 to 1000.
+    Args:
+        min (float): The minimum value of the range.
+        max (float): The maximum value of the range.
+        value (float): The value to be normalized.
+    Returns:
+        float: The normalized value scaled to the range 0 to 1000.
+    """
+    return ((value - min) / (max - min)) * 1000
